198be7dab7e72072f5d0b1f1faf74c6d0239cfab
Justin Smith <noisesmith@gmail.com>
2014-01-03 15:00:59 -0800

    version bump for deploy

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject caribou/caribou-plugin "0.2.1"
+(defproject caribou/caribou-plugin "0.2.2"
   :description "A coordinator of plugged in extension state."
   :dependencies [[caribou/caribou-core "0.13.0"]
                  [caribou/caribou-frontend "0.13.3"]]
